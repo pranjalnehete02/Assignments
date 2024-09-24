05440526690c6ce2b770008a9e29aad25234e352
Pranjal Nehete <pranjalnehete02@gmail.com>
2024-09-24 22:46:16 +0530

    Kuldeep_Demo

---
+++ Java/MyPractice/Day/src/Hello.java
@@ -0,0 +1,12 @@
+
+
+
+package com.cdac.acts;
+public class Hello{
+
+    public static void hi(){
+        System.out.println("Hii");
+    }
+
+
+    }
---
+++ Java/MyPractice/Day/src/call.java
@@ -0,0 +1,7 @@
+package com.cdac.iscr;
+import com.cdac.acts.Hello;
+public class call {
+    public static void main(String[] args) {
+       Hello.hi();
+    }    
+}

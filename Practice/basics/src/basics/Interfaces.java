8d85ec3264319768cd6342dd173ed0f78b5a83f8
Pranjal Nehete <pranjalnehete02@gmail.com>
2024-10-11 01:32:10 +0530

    Interface

---
+++ Practice/basics/src/basics/Interfaces.java
@@ -0,0 +1,35 @@
+package basics;
+
+
+interface Animal{
+	void makeSonnd();
+}
+
+
+class dog implements Animal{
+
+	@Override
+	public void makeSonnd() {
+		System.out.println("My sound is to barkk !!!");
+		
+	}
+}
+
+class cat implements Animal{
+
+	@Override
+	public void makeSonnd() {
+		System.out.println("My sound is maauuwww !!");
+		
+	}
+	
+}
+
+public class Interfaces {
+		public static void main(String[] args) {
+			dog d = new dog();
+			cat c = new cat();
+			d.makeSonnd();
+			c.makeSonnd();
+		}
+}
